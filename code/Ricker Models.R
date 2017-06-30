0386f3853fef7983e4bcccca629e73fa1b1c6c84
rich-brenner <richard.brenner@alaska.gov>
2017-06-30 14:52:45 -0800

    added Sara's changes

--- code/Ricker Models.R
+++ code/Ricker Models.R
@@ -1,15 +1,13 @@
-
 #Original code was from Steve Fleischman and Xinxian Zhang. code was modified
 #and converted to JAGS by Sara Miller on 28 June 2017 and maintained
 #Rich Brenner.
-
 
 #load----
 library(arm)
 library(lmtest)
 library(rjags)
-
-
+library(R2OpenBUGS)
+library(gdata)
 #data----
 brood<-read.table("data/Chilkoot_Sock.csv", header=TRUE, sep=",")
 sr<-brood[,2:3] #retrieve spawner recruit data for one stock
@@ -40,7 +38,7 @@ Ricker=function(){
   for(y in 1:n) {lnRS[y] ~ dnorm(mean2.lnRS[y],tau.white) }
   
   mean2.lnRS[1]     <- mean1.lnRS[1] + phi * resid.red.0  
-  for (y in 2:n) { mean2.lnRS[y] <- mean1.lnRS[y] + phi * resid.red[y-1] }   #non-AR model
+  for (y in 2:n) { mean2.lnRS[y] <- mean1.lnRS[y] + phi * resid.red[y-1] }  #no autocorrelation model
   
   for(y in 1:n) {  mean1.lnRS[y] <- lnalpha - beta * S[y]  }
   for(y in 1:n) {  resid.red[y]     <- lnRS[y] - mean1.lnRS[y]  }
@@ -113,7 +111,7 @@ AR=function(){
   
   S.star[1]<-0
   step<-400
-  for (i in 2:501) {                      #LOOP TO FIND Pr(SY>90%MSY)
+  for (i in 2:501) {                      #LOOP TO FIND Pr(SY>90%MSY) #not sure how to use results from this
     S.star[i] <- S.star[i-1]+step
     R.star[i] <- S.star[i] * exp(lnalpha.c - beta * S.star[i]) 
     SY[i] <- R.star[i] - S.star[i]
@@ -126,20 +124,19 @@ model_file_loc=paste("code/Chilkoot_Sockeye_AR.txt", sep="")
 write.model(AR, paste("code/Chilkoot_Sockeye_AR.txt", sep=""))
 
 #results----
+#100000 iterations, 3 chains, 10000 burn-in period, thin by 100
 #Run Ricker without AR(1)
 inits1<-list(lnalpha=1.5, beta=0.0005, sigma.white=0.7, resid.red.0= 0)
 inits2<-list(lnalpha=2.0, beta=0.0010, sigma.white=0.5, resid.red.0=-1)
 inits3<-list(lnalpha=2.5, beta=0.0020, sigma.white=0.3, resid.red.0= 1)
 inits<-list(inits1, inits2, inits3)
-parameters<-c("lnalpha","beta", "sigma.red","S.msy","MSY", "I90")
+#parameters<-c("lnalpha","beta", "sigma.red","S.msy","MSY", "I90")
+parameters<-c("lnalpha","beta", "sigma.red","S.msy","MSY")
 ptm = proc.time()
 jmod <- jags.model(file='code/Chilkoot_Sockeye.txt', data=sr.data, n.chains=3, inits=inits, n.adapt=1000) 
-x<-update(jmod, n.iter=20000, by=5, progress.bar='text', DIC=T, n.burnin=1000) 
-post <- coda.samples(jmod, parameters, n.iter=20000, thin=5, n.burnin=1000)
+x<-update(jmod, n.iter=100000, by=100, progress.bar='text', DIC=T, n.burnin=10000) 
+post <- coda.samples(jmod, parameters, n.iter=100000, thin=100, n.burnin=10000)
 post.samp <- post
-save(post.samp, file = "results/coda.samples.Ricker.RData")
-outJG<-as.matrix(post.samp)
-write.csv(outJG, file = "results/coda.samples.Ricker.csv") #save mcmc list
 
 #Numerical summary of each parameter (mean, median, quantiles)
 summary<-summary(post)                     
@@ -170,19 +167,45 @@ for(j in seq(1,nvars,int)){
   }}
 dev.off()
 
+
+#Gelman
+gel <- as.data.frame(gelman.diag(post, multivariate=F)[[1]])
+poor.threshold=1.10#values less than 1.2 are generally considered converged
+poor <- gel[gel[,1] > poor.threshold, ]
+write.csv(poor, file= paste("results/Ricker_Gelman.csv") )    
+
+#DIC
+dic.pD  <-dic.samples(jmod,n.iter=100000, thin=100,"pD",  n.burnin=10000)
+dic.popt<-dic.samples(jmod,n.iter=100000, thin=100,"popt",n.burnin=10000)
+dev1 <- sum(dic.pD[[1]])
+pD   <- sum(dic.pD[[2]])
+dic.pD <- dev1 + pD
+dic.pD.summary <- data.frame(dev1, pD, dic.pD)
+write.csv(dic.pD.summary, file=paste("results/Ricker_DIC.csv") ) 
+
+#Create coda samples for horsetail plots and probability plots
+post2 <- coda.samples(jmod, c("lnalpha", "beta", "lnalpha.c"), n.iter=100000, thin=10,n.burnin=10000) 
+x <- as.array(post2)
+x <- data.frame(x)
+coda <- x[,1:3]
+coda<- rename.vars(coda, from=c("beta.1","lnalpha.1","lnalpha.c.1"), to=c("beta","lnalpha", "lnalpha.c"))
+write.csv(coda, file= paste("results/Ricker_coda.csv") ,row.names=FALSE)    # writes csv file
+
+###########################################################################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##########################################################################################################
 #Run Ricker with AR(1):  
+#100000 iterations, 3 chains, 10000 burn-in period, thin by 100
 inits1<-list(lnalpha=1.5, beta=0.0005, phi= 0.3, sigma.white=0.7, resid.red.0= 0)
 inits2<-list(lnalpha=2.0, beta=0.0010, phi=-0.1, sigma.white=0.5, resid.red.0=-1)
 inits3<-list(lnalpha=2.5, beta=0.0020, phi= 0.2, sigma.white=0.3, resid.red.0= 1)
 inits<-list(inits1, inits2, inits3)
-parameters<-c("lnalpha.c","beta", "sigma.red","S.msy", "MSY", "I90" )
+#parameters<-c("lnalpha.c","beta", "sigma.red","S.msy", "MSY", "I90" )
+parameters<-c("lnalpha.c","beta", "sigma.red","S.msy", "MSY", "phi")
 jmod <- jags.model(file='code/Chilkoot_Sockeye_AR.txt', data=sr.data, n.chains=3, inits=inits, n.adapt=1000) 
-x<-update(jmod, n.iter=20000, by=5, progress.bar='text', DIC=T, n.burnin=1000) 
-post <- coda.samples(jmod, parameters, n.iter=20000, thin=5, n.burnin=1000)
+x<-update(jmod, n.iter=100000, by=100, progress.bar='text', DIC=T, n.burnin=10000) 
+post <- coda.samples(jmod, parameters, n.iter=100000, thin=100, n.burnin=10000)
 post.samp <- post
-save(post.samp, file = "results/coda.samples.Ricker_AR.RData")
-outJG<-as.matrix(post.samp)
-write.csv(outJG, file = "results/coda.samples.Ricker_AR.csv") 
 
 #Numerical summary of each parameter (mean, median, quantiles)
 summary<-summary(post)                     
@@ -213,3 +236,25 @@ for(j in seq(1,nvars,int)){
   }}
 dev.off()
 
+#Gelman
+gel <- as.data.frame(gelman.diag(post, multivariate=F)[[1]])
+poor.threshold=1.10#values less than 1.2 are generally considered converged
+poor <- gel[gel[,1] > poor.threshold, ]
+write.csv(poor, file= paste("results/Ricker_AR_Gelman.csv") )    
+
+#DIC
+dic.pD  <-dic.samples(jmod,n.iter=100000, thin=100,"pD",  n.burnin=10000)
+dic.popt<-dic.samples(jmod,n.iter=100000, thin=100,"popt",n.burnin=10000)
+dev1 <- sum(dic.pD[[1]])
+pD   <- sum(dic.pD[[2]])
+dic.pD <- dev1 + pD
+dic.pD.summary <- data.frame(dev1, pD, dic.pD)
+write.csv(dic.pD.summary, file=paste("results/Ricker_AR_DIC.csv") ) 
+
+#Create coda samples for horsetail plots and probability plots
+post2 <- coda.samples(jmod, c("lnalpha", "beta", "lnalpha.c"), n.iter=100000, thin=10,n.burnin=10000) 
+x <- as.array(post2)
+x <- data.frame(x)
+coda <- x[,1:3]
+coda<- rename.vars(coda, from=c("beta.1","lnalpha.1","lnalpha.c.1"), to=c("beta","lnalpha", "lnalpha.c"))
+write.csv(coda, file= paste("results/Ricker_AR_coda.csv") ,row.names=FALSE) # writes csv file
