2465f311d7e1d50f3cd98ee9f52a595819f45103
Ben Williams <ben.williams@alaska.gov>
2017-07-25 17:12:42 -0800

    cleanup code

--- code/Ricker Models.R
+++ code/Ricker Models.R
@@ -11,26 +11,29 @@ library(gdata)
 library(tidyverse)
 
 #data----
-brood <- read.table("data/Chilkoot_Sock.csv", header=TRUE, sep=",")
-sr <- brood[,2:3] #retrieve spawner recruit data from rows 2-3 of the brood dataset.
-colnames(sr) <- c("S","R") #rename the column names
-sr <- na.omit(sr)  #remove missing rows with missing values(na)
-sr$lnRS<-log(sr$R/sr$S) #add one column called lnRS
-n <- nrow(sr) #calculates the number of years of data.
-sr.data<-list(n=n,S=sr$S, lnRS=sr$lnRS)
+brood <- read_csv("data/Chilkoot_Sock.csv")
 
+# cleanup data
+brood %>% 
+  dplyr::select(S=Spawn, R=Recruit) %>% 
+  mutate(lnRS = log(R/S), n()) -> sr
+
+n <- nrow(sr) #calculates the number of years of data.
+sr.data <- list(n = n, S = sr$S, lnRS = sr$lnRS)
 
 #analysis----
 #check AR(1)
-dwtest(sr$lnRS ~ sr$S)
 mylm <- lm(lnRS ~ S, sr)
+dwtest(mylm)
 pacf(residuals(mylm))
 
 
-##Ricker model for stock-recruitment analysis ##################################################
-#Created by Steve Fleischman  ##################################################################
-#First, Ricker WITHOUT autocorrelation #########################################################
-Ricker=function(){
+# JAGS CODE NOT R CODE
+# Ricker w/o autocorrelation ----
+# Ricker model for stock-recruitment analysis 
+# Created by Steve Fleischman  
+
+Ricker = function(){
   
   lnalpha ~ dunif(0, 10) 
   beta ~ dunif(0, 10)                 
@@ -72,14 +75,15 @@ Ricker=function(){
     I90[i] <- step(SY[i] - 0.9 * MSY)  
   }
 }
+
 #write the non-AR model to a text file to be called by WinBUGS
 model_file_loc=paste("code/Chilkoot_Sockeye.txt", sep="")
 write.model(Ricker, paste("code/Chilkoot_Sockeye.txt", sep=""))
 
 
-#############################################################################
-#Next, Ricker model WITH autocorrelation  ###################################
-AR=function(){
+
+# Ricker model WITH autocorrelation  ----
+AR = function(){
   
   lnalpha ~ dunif(0, 10)
   beta ~ dunif(0, 10) 
@@ -128,20 +132,19 @@ AR=function(){
 model_file_loc=paste("code/Chilkoot_Sockeye_AR.txt", sep="")
 write.model(AR, paste("code/Chilkoot_Sockeye_AR.txt", sep=""))
 
+# NOW BACK TO R CODE
 
+# Results ----
+# 100000 iterations, 3 chains, 10000 burn-in period, thin by 100
+# Run the Ricker model that does NOT have autocorrelation
 
-#######################################################################################
-#######################################################################################
-#results
-#100000 iterations, 3 chains, 10000 burn-in period, thin by 100
-#Run the Ricker model that does NOT have autocorrelation
 inits1 <- list(lnalpha=1.5, beta=0.0005, sigma.white=0.7, resid.red.0= 0)
 inits2 <- list(lnalpha=2.0, beta=0.0010, sigma.white=0.5, resid.red.0=-1)
 inits3 <- list(lnalpha=2.5, beta=0.0020, sigma.white=0.3, resid.red.0= 1)
 inits <- list(inits1, inits2, inits3)
 
-#parameters<-c("lnalpha","beta", "sigma.red","S.msy","MSY", "I90") No phi????
-parameters<-c("lnalpha","beta", "sigma.red","S.msy","MSY")
+#parameters <- c("lnalpha","beta", "sigma.red","S.msy","MSY", "I90") No phi????
+parameters <- c("lnalpha","beta", "sigma.red","S.msy","MSY")
 ptm = proc.time()
 jmod <- jags.model(file='code/Chilkoot_Sockeye.txt', data=sr.data, n.chains=3, inits=inits, n.adapt=1000) 
 x <- update(jmod, n.iter=100000, by=100, progress.bar='text', DIC=T, n.burnin=10000) 
@@ -149,19 +152,24 @@ post <- coda.samples(jmod, parameters, n.iter=100000, thin=100, n.burnin=10000)
 post.samp <- post
 
 #Numerical summary of each parameter (mean, median, quantiles)
+
 summary <- summary(post)                     
 stats <- summary$statistics;  colnames(stats)
 quants <- summary$quantiles;  colnames(quants)
 statsquants <- cbind(stats,quants) 
 statsquants <- statsquants[,c(1,2,4,5,7,9)] #select columns of interest
-write.csv(statsquants, file= paste("results/Ricker.csv") )    
 
-#Density and time series plots
+write_csv(statsquants, "results/Ricker.csv")     
+
+# Plots ----
+# Density and time series plots
 post.samp <- post
 nvars <- dim(post.samp[[1]])[2]
 nsamps <- dim(post.samp[[1]])[1]
 int <- 25
+
 pdf("figures/Ricker_profiles.pdf",height=6, width=8)
+
 for(j in seq(1,nvars,int)){
   par(mfrow=c(5,4),mai=c(0.3,0.3,0.2,0.2))
   for(i in 0:(int-1)){
@@ -175,23 +183,26 @@ for(j in seq(1,nvars,int)){
     lines(as.numeric(post.samp[[2]][,i+j]),col='red')
     lines(density(post.samp[[3]][,i+j]),col='green')
   }}
+
 dev.off()
 
 
-#Gelman
+# Gelman ----
 gel <- as.data.frame(gelman.diag(post, multivariate=F)[[1]])
 poor.threshold=1.10#values less than 1.2 are generally considered converged
 poor <- gel[gel[,1] > poor.threshold, ]
-write.csv(poor, file= paste("results/Ricker_Gelman.csv") )    
 
-#DIC
+write_csv(poor, "results/Ricker_Gelman.csv")     
+
+# DIC ----
 dic.pD  <-dic.samples(jmod,n.iter=100000, thin=100,"pD",  n.burnin=10000)
 dic.popt<-dic.samples(jmod,n.iter=100000, thin=100,"popt",n.burnin=10000)
 dev1 <- sum(dic.pD[[1]])
 pD   <- sum(dic.pD[[2]])
 dic.pD <- dev1 + pD
 dic.pD.summary <- data.frame(dev1, pD, dic.pD)
-write.csv(dic.pD.summary, file=paste("results/Ricker_DIC.csv") ) 
+
+write_csv(dic.pD.summary, "results/Ricker_DIC.csv")  
 
 #Create coda samples for horsetail plots and probability plots
 post2 <- coda.samples(jmod, c("lnalpha", "beta", "lnalpha.c"), n.iter=100000, thin=10,n.burnin=10000) 
@@ -199,27 +210,26 @@ x <- as.array(post2)
 x <- data.frame(x)
 coda <- x[,1:3]
 coda <- rename.vars(coda, from=c("beta.1","lnalpha.1","lnalpha.c.1"), to=c("beta","lnalpha", "lnalpha.c"))
-write.csv(coda, file= paste("results/Ricker_coda.csv") ,row.names=FALSE)    # writes csv file
+
+write_csv(coda, "results/Ricker_coda.csv")  # writes csv file
 
 
-###########################################################################################################
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-##########################################################################################################
-#Run Ricker WITH AR(1):  
-#100000 iterations, 3 chains, 10000 burn-in period, thin by 100
+
+# Run Ricker WITH AR(1) ----  
+# 100000 iterations, 3 chains, 10000 burn-in period, thin by 100
 inits1 <- list(lnalpha=1.5, beta=0.0005, phi= 0.3, sigma.white=0.7, resid.red.0= 0)
 inits2 <- list(lnalpha=2.0, beta=0.0010, phi=-0.1, sigma.white=0.5, resid.red.0=-1)
 inits3 <- list(lnalpha=2.5, beta=0.0020, phi= 0.2, sigma.white=0.3, resid.red.0= 1)
 inits <- list(inits1, inits2, inits3)
 
-#parameters<-c("lnalpha.c","beta", "sigma.red","S.msy", "MSY", "I90" )
+# parameters<-c("lnalpha.c","beta", "sigma.red","S.msy", "MSY", "I90" )
 parameters <- c("lnalpha.c","beta", "sigma.red","S.msy", "MSY", "phi", "S.max", "S.eq", "S.msy", "U.msy", "R.msy")
 jmod <- jags.model(file='code/Chilkoot_Sockeye_AR.txt', data=sr.data, n.chains=3, inits=inits, n.adapt=1000) 
 x <- update(jmod, n.iter=100000, by=100, progress.bar='text', DIC=T, n.burnin=10000) 
 post <- coda.samples(jmod, parameters, n.iter=100000, thin=100, n.burnin=10000)
 post.samp <- post
 
-#Numerical summary of each parameter (mean, median, quantiles)
+# Numerical summary of each parameter (mean, median, quantiles)
 summary <- summary(post)                     
 stats <- summary$statistics;  colnames(stats)
 quants <- summary$quantiles;  colnames(quants)
@@ -229,13 +239,13 @@ statsquants <- statsquants[,c(1,2,4,5,7,9)] #select statquant columns of interes
 data.frame(statsquants) %>%  #converts statsquants to a dataframe
   tibble::rownames_to_column() %>%  #preserves the row names (otherwise, tibble  drops these)
   mutate(CV = SD/Mean) %>% #calculates the CV
-  rename(Item=rowname, "Time series SE" = Time.series.SE, "2.5%"=X2.5.)-> statsquants #converting to a tibble messed
+  rename(Item=rowname, "Time series SE" = Time.series.SE, "2.5%"=X2.5.) -> statsquants #converting to a tibble messed
                                                                                       #the column names, need
                                                                                       #to rename these
 
 write.csv(statsquants, file= paste("results/Ricker_AR.csv") ) 
 
-#Density and time series plots
+# Density and time series plots
 post.samp <- post
 nvars <- dim(post.samp[[1]])[2]
 nsamps <- dim(post.samp[[1]])[1]
@@ -256,20 +266,21 @@ for(j in seq(1,nvars,int)){
   }}
 dev.off()
 
-#Gelman
+# Gelman ----
 gel <- as.data.frame(gelman.diag(post, multivariate=F)[[1]])
 poor.threshold=1.10 #values less than 1.2 are generally considered converged
-poor <- gel[gel[,1] > poor.threshold, ]
-write.csv(poor, file= paste("results/Ricker_AR_Gelman.csv") )    
+poor <- gel[gel[,1] > poor.threshold,]
 
-#DIC
-dic.pD  <-dic.samples(jmod,n.iter=100000, thin=100,"pD",  n.burnin=10000)
-dic.popt<-dic.samples(jmod,n.iter=100000, thin=100,"popt",n.burnin=10000)
+write_csv(poor, "results/Ricker_AR_Gelman.csv")     
+
+# DIC ----
+dic.pD  <- dic.samples(jmod,n.iter=100000, thin=100,"pD",  n.burnin=10000)
+dic.popt <- dic.samples(jmod,n.iter=100000, thin=100,"popt",n.burnin=10000)
 dev1 <- sum(dic.pD[[1]])
 pD   <- sum(dic.pD[[2]])
 dic.pD <- dev1 + pD
 dic.pD.summary <- data.frame(dev1, pD, dic.pD)
-write.csv(dic.pD.summary, file=paste("results/Ricker_AR_DIC.csv") ) 
+write_csv(dic.pD.summary, "results/Ricker_AR_DIC.csv")  
 
 #Create coda samples for horsetail plots and probability plots for the AR model
 post2 <- coda.samples(jmod, c("lnalpha", "beta", "lnalpha.c"), n.iter=100000, thin=10,n.burnin=10000) 
@@ -277,5 +288,5 @@ x <- as.array(post2)
 x <- data.frame(x)
 coda <- x[,1:3] 
 coda <- rename.vars(coda, from=c("beta.1","lnalpha.1","lnalpha.c.1"), to=c("beta","lnalpha", "lnalpha.c"))
-write.csv(coda, file= paste("results/Ricker_AR_coda.csv") ,row.names=FALSE) # writes csv file
+write_csv(coda, "results/Ricker_AR_coda.csv") # writes csv file
 
